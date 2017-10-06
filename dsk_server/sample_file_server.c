a5bca40d4e169b77aeb6a37da1ab641c472fd05d
Prateek Vij <prateekvij55@gmail.com>
2017-10-06 17:14:01 +0530

    Added disk folders for server and client for storing sample files for testing. Few sample files already added

---
+++ dsk_server/sample_file_server.c
@@ -0,0 +1,121 @@
+closed gracefully by releasing the socket resource.
+I am from server
+In this assignment, you require to implement two C programs, namely server and client to communicate
+with each other based on TCP sockets. The aim is to implement simple Base64 encoding communication
+protocol.
+Initially, server will be waiting for a TCP connection from the client. Then, client will connect to the
+server using server’s TCP port already known to the client. After successful connection, the client accepts
+the text input from the user and encodes the input using Base64 encoding system. Once encoded message
+is computed the client sends the Message (Type 1 message) to the server via TCP port. After receiving the
+Message, server should print the received and original message by decoding the received message, and
+sends an ACK (Type 2 message) to the client. The client and server should remain in a loop to
+communicate any number of messages. Once the client wants to close the communication, it should send
+a Message (Type 3 Message) to the server and the TCP connection on both the server and client should be
+closed gracefully by releasing the socket resource.
+In this assignment, you require to implement two C programs, namely server and client to communicate
+with each other based on TCP sockets. The aim is to implement simple Base64 encoding communication
+protocol.
+Initially, server will be waiting for a TCP connection from the client. Then, client will connect to the
+server using server’s TCP port already known to the client. After successful connection, the client accepts
+the text input from the user and encodes the input using Base64 encoding system. Once encoded message
+is computed the client sends the Message (Type 1 message) to the server via TCP port. After receiving the
+Message, server should print the received and original message by decoding the received message, and
+sends an ACK (Type 2 message) to the client. The client and server should remain in a loop to
+communicate any number of messages. Once the client wants to close the communication, it should send
+a Message (Type 3 Message) to the server and the TCP connection on both the server and client should be
+closed gracefully by releasing the socket resource.
+closed gracefully by releasing the socket resource.
+
+closed gracefully by releasing the socket resource.
+
+closed gracefully by releasing the socket resource.
+I am from server
+In this assignment, you require to implement two C programs, namely server and client to communicate
+with each other based on TCP sockets. The aim is to implement simple Base64 encoding communication
+protocol.
+Initially, server will be waiting for a TCP connection from the client. Then, client will connect to the
+server using server’s TCP port already known to the client. After successful connection, the client accepts
+the text input from the user and encodes the input using Base64 encoding system. Once encoded message
+is computed the client sends the Message (Type 1 message) to the server via TCP port. After receiving the
+Message, server should print the received and original message by decoding the received message, and
+sends an ACK (Type 2 message) to the client. The client and server should remain in a loop to
+communicate any number of messages. Once the client wants to close the communication, it should send
+a Message (Type 3 Message) to the server and the TCP connection on both the server and client should be
+closed gracefully by releasing the socket resource.
+In this assignment, you require to implement two C programs, namely server and client to communicate
+with each other based on TCP sockets. The aim is to implement simple Base64 encoding communication
+protocol.
+Initially, server will be waiting for a TCP connection from the client. Then, client will connect to the
+server using server’s TCP port already known to the client. After successful connection, the client accepts
+the text input from the user and encodes the input using Base64 encoding system. Once encoded message
+is computed the client sends the Message (Type 1 message) to the server via TCP port. After receiving the
+Message, server should print the received and original message by decoding the received message, and
+sends an ACK (Type 2 message) to the client. The client and server should remain in a loop to
+communicate any number of messages. Once the client wants to close the communication, it should send
+a Message (Type 3 Message) to the server and the TCP connection on both the server and client should be
+closed gracefully by releasing the socket resource.
+closed gracefully by releasing the socket resource.
+
+closed gracefully by releasing the socket resource.
+
+closed gracefully by releasing the socket resource.
+I am from server
+In this assignment, you require to implement two C programs, namely server and client to communicate
+with each other based on TCP sockets. The aim is to implement simple Base64 encoding communication
+protocol.
+Initially, server will be waiting for a TCP connection from the client. Then, client will connect to the
+server using server’s TCP port already known to the client. After successful connection, the client accepts
+the text input from the user and encodes the input using Base64 encoding system. Once encoded message
+is computed the client sends the Message (Type 1 message) to the server via TCP port. After receiving the
+Message, server should print the received and original message by decoding the received message, and
+sends an ACK (Type 2 message) to the client. The client and server should remain in a loop to
+communicate any number of messages. Once the client wants to close the communication, it should send
+a Message (Type 3 Message) to the server and the TCP connection on both the server and client should be
+closed gracefully by releasing the socket resource.
+In this assignment, you require to implement two C programs, namely server and client to communicate
+with each other based on TCP sockets. The aim is to implement simple Base64 encoding communication
+protocol.
+Initially, server will be waiting for a TCP connection from the client. Then, client will connect to the
+server using server’s TCP port already known to the client. After successful connection, the client accepts
+the text input from the user and encodes the input using Base64 encoding system. Once encoded message
+is computed the client sends the Message (Type 1 message) to the server via TCP port. After receiving the
+Message, server should print the received and original message by decoding the received message, and
+sends an ACK (Type 2 message) to the client. The client and server should remain in a loop to
+communicate any number of messages. Once the client wants to close the communication, it should send
+a Message (Type 3 Message) to the server and the TCP connection on both the server and client should be
+closed gracefully by releasing the socket resource.
+closed gracefully by releasing the socket resource.
+
+closed gracefully by releasing the socket resource.
+
+closed gracefully by releasing the socket resource.
+I am from server
+In this assignment, you require to implement two C programs, namely server and client to communicate
+with each other based on TCP sockets. The aim is to implement simple Base64 encoding communication
+protocol.
+Initially, server will be waiting for a TCP connection from the client. Then, client will connect to the
+server using server’s TCP port already known to the client. After successful connection, the client accepts
+the text input from the user and encodes the input using Base64 encoding system. Once encoded message
+is computed the client sends the Message (Type 1 message) to the server via TCP port. After receiving the
+Message, server should print the received and original message by decoding the received message, and
+sends an ACK (Type 2 message) to the client. The client and server should remain in a loop to
+communicate any number of messages. Once the client wants to close the communication, it should send
+a Message (Type 3 Message) to the server and the TCP connection on both the server and client should be
+closed gracefully by releasing the socket resource.
+In this assignment, you require to implement two C programs, namely server and client to communicate
+with each other based on TCP sockets. The aim is to implement simple Base64 encoding communication
+protocol.
+Initially, server will be waiting for a TCP connection from the client. Then, client will connect to the
+server using server’s TCP port already known to the client. After successful connection, the client accepts
+the text input from the user and encodes the input using Base64 encoding system. Once encoded message
+is computed the client sends the Message (Type 1 message) to the server via TCP port. After receiving the
+Message, server should print the received and original message by decoding the received message, and
+sends an ACK (Type 2 message) to the client. The client and server should remain in a loop to
+communicate any number of messages. Once the client wants to close the communication, it should send
+a Message (Type 3 Message) to the server and the TCP connection on both the server and client should be
+closed gracefully by releasing the socket resource.
+closed gracefully by releasing the socket resource.
+
+closed gracefully by releasing the socket resource.
+
+closed gracefully by releasing the socket resource.
